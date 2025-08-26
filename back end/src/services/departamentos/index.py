5a684feafb4df079b2e13281baeb2ba8e91194e4
jaonunesunb <jaonunesunb@gmail.com>
2025-08-26 16:31:19 -0300

    updates

--- back end/src/services/departamentos/index.py
+++ back end/src/services/departamentos/index.py
@@ -1,20 +1,14 @@
-from src.db_connection.connection import get_db_connection
+from src.db_connection.connection import get_cursor
 
 def create_departamento(nome):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
     insert_query = '''
         INSERT INTO Departamentos (nome)
         VALUES (%s)
         RETURNING id
     '''
-    cursor.execute(insert_query, (nome,))
-    departamento_id = cursor.fetchone()[0]
-    conn.commit()
-
-    cursor.close()
-    conn.close()
+    with get_cursor() as cursor:
+        cursor.execute(insert_query, (nome,))
+        departamento_id = cursor.fetchone()[0]
 
     return {
         'departamento_id': departamento_id,
@@ -22,33 +16,23 @@ def create_departamento(nome):
     }
 
 def get_departamentos():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
     select_query = '''
         SELECT id, nome FROM Departamentos
     '''
-    cursor.execute(select_query)
-    departamentos = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
+    with get_cursor() as cursor:
+        cursor.execute(select_query)
+        departamentos = cursor.fetchall()
 
     departamentos_list = [{'departamento_id': departamento[0], 'nome': departamento[1]} for departamento in departamentos]
     return departamentos_list
 
 def get_departamento_by_id(departamento_id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
     select_query = '''
         SELECT id, nome FROM Departamentos WHERE id = %s
     '''
-    cursor.execute(select_query, (departamento_id,))
-    departamento = cursor.fetchone()
-
-    cursor.close()
-    conn.close()
+    with get_cursor() as cursor:
+        cursor.execute(select_query, (departamento_id,))
+        departamento = cursor.fetchone()
 
     if departamento:
         departamento_data = {'departamento_id': departamento[0], 'nome': departamento[1]}
@@ -57,23 +41,14 @@ def get_departamento_by_id(departamento_id):
         return None
 
 def get_departamento_name(departamento_id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
     select_query = '''
         SELECT nome FROM Departamentos WHERE id = %s
     '''
-    cursor.execute(select_query, (departamento_id,))
-    departamento = cursor.fetchone()
-
-    cursor.close()
-    conn.close()
+    with get_cursor() as cursor:
+        cursor.execute(select_query, (departamento_id,))
+        departamento = cursor.fetchone()
 
     if departamento:
         return departamento[0]
     else:
         return None
-
-
-
-
